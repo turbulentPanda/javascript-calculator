d4d131a8f8449a03a9f96f02ed265629a04e1b99
Christopher Marchant <cmarch240@gmail.com>
2021-08-03 00:29:39 -0500

    Added functions for all arithmetic operators (add, subtract, multiply, divide, exponentiation, power, factorial, getPi, negateNumber)

--- scripts.js
+++ scripts.js
@@ -23,3 +23,54 @@ function toggleLightMode() {
     result.classList.toggle('light-mode');
 
 }
+
+// ************** Operation Functions **************
+function add(number1, number2) {
+    return number1 + number2;
+}
+
+function subtract(number1, number2) {
+    return number1 - number2;
+}
+
+function multiply(number1, number2) {
+    return number1 * number2;
+}
+
+function divide(number1, number2) {
+    if (number2 === 0) {
+        return "Did you REALLY divide by 0?"
+    } else {
+        return number1 / number2;
+    }
+}
+
+function exponentiation(number1) {
+    return Math.E ** number1;
+}
+
+function power(number1, number2) {
+    return number1 ** number2;
+}
+
+function factorial(number1) {
+    if (number1 === 0) {
+        return 1;
+    } else if (number1 === 1) {
+        return 1;
+    } else {
+        return number1 * factorial(number1 - 1);
+    }
+}
+
+function getPi() {
+    return 3.14159;
+}
+
+function negateNumber(number1) {
+    if (number1 === 0) {
+        return 0;
+    } else {
+        return -number1;
+    }
+}
